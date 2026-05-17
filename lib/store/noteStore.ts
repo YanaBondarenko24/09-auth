9bbbd4d38416357adcb5d28de2dd14221ff00e53
Yana Bondarenko <yana-lyana27@mail.ru>
2026-05-17 15:00:43 +0200

    fix

--- lib/store/noteStore.ts
+++ lib/store/noteStore.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
-import { NoteFormValues } from "@/components/NoteForm/NoteForm";
+import { NoteFormValues } from "@/types/note";
 
 
 
